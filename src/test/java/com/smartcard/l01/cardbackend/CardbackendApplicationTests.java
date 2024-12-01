66082246db3c161abecddb07de638bcc7253f348
Dao Huy Vu <huyvu160102@gmail.com>
2024-12-01 15:25:53 +0700

    Initial commit

---
+++ src/test/java/com/smartcard/l01/cardbackend/CardbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcard.l01.cardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
